726c2abb92d8c61271693ce0cbe682b85fa48577
Miguel de Icaza <miguel@gnome.org>
2002-12-09 23:15:26 +0000

    Commit this while Gonzalo sleeps
    svn path=/trunk/xsp/; revision=9504

--- server/server.cs
+++ server/server.cs
@@ -17,25 +17,47 @@ namespace Mono.ASPNET
 {
 	public class Server
 	{
+		public static void ShowHelp ()
+		{
+			Console.WriteLine ("XSP server is a sample server that hosts the ASP.NET runtime in a\n");
+			Console.WriteLine ("minimalistic HTTP server\n\n");
+			Console.WriteLine ("Usage is:\n");
+			Console.WriteLine ("    xsp.exe [--port N]");
+		}
+		
 		public static int Main (string [] args)
 		{
+			object oport;
+			
 			Trace.Listeners.Add (new TextWriterTraceListener (Console.Out));
 			Type type = typeof (MonoApplicationHost);
 			string cwd = Directory.GetCurrentDirectory ();
 			MonoApplicationHost host;
 
 			host =  (MonoApplicationHost) ApplicationHost.CreateApplicationHost (type, "/", cwd);
-			object o = ConfigurationSettings.AppSettings ["MonoServerPort"];
-			if (o == null) {
-				o = 8080;
-			}
+			oport = ConfigurationSettings.AppSettings ["MonoServerPort"];
+			if (oport == null)
+				oport = 8080;
 
+			for (int i = 0; i < args.Length; i++){
+				string a = args [i];
+				
+				switch (a){
+				case "--port":
+					oport = args [++i];
+					break;
+				case "--help":
+					ShowHelp ();
+					return 0;
+				}
+			}
+			
 			ushort port;
 
 			try {
-				port = Convert.ToUInt16 (o);
+				port = Convert.ToUInt16 (oport);
 			} catch (Exception e) {
-				Console.WriteLine ("The value given for the listen port is not valid: " + o);
+				Console.WriteLine ("The value given for the listen port is not valid: " + oport);
 				return 1;
 			}
 			

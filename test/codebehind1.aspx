5f9264b9b202ba197074988c7c1156b2ba6c47e8
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-01-06 03:02:01 +0000

    2003-01-06  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* Makefile: added codebehind1 test.
    	* codebehind1.[aspx|cs]: codebehind test by David B. Bitton
    	<david@codenoevil.com>.
    
    svn path=/trunk/xsp/; revision=10196

---
+++ test/codebehind1.aspx
@@ -0,0 +1,21 @@
+<%@ Page language="c#" Inherits="MonoTest.Test" %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" >
+<HTML>
+	<HEAD>
+		<title>Test</title>
+		<meta name="GENERATOR" Content="Microsoft Visual Studio 7.0">
+		<meta name="CODE_LANGUAGE" Content="C#">
+		<meta name="vs_defaultClientScript" content="JavaScript">
+		<meta name="vs_targetSchema" content="http://schemas.microsoft.com/intellisense/ie5">
+	</HEAD>
+	<body>
+		<form id="Test" method="post" runat="server">
+			<P>
+				<asp:Label id="Label1" runat="server">Text will go here.</asp:Label></P>
+			<P>
+				<asp:TextBox id="TextBox1" runat="server"></asp:TextBox></P>
+			<P>
+				<asp:Button id="SubmitButton" runat="server" Text="Submit"></asp:Button></P>
+		</form>
+	</body>
+</HTML>
---
+++ test/codebehind1.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace MonoTest
+{
+	/// <summary>
+	/// Summary description for Test.
+	/// </summary>
+	public class Test : System.Web.UI.Page
+	{
+		protected System.Web.UI.WebControls.Label Label1;
+		protected System.Web.UI.WebControls.Button SubmitButton;
+		protected System.Web.UI.WebControls.TextBox TextBox1;
+	
+		private void Page_Load(object sender, System.EventArgs e)
+		{
+			Label1.Text = "Page Loaded";
+		}
+
+		#region Web Form Designer generated code
+		override protected void OnInit(EventArgs e)
+		{
+			//
+			// CODEGEN: This call is required by the ASP.NET Web Form Designer.
+			//
+			InitializeComponent();
+			base.OnInit(e);
+		}
+		
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{    
+			this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+			this.Load += new System.EventHandler(this.Page_Load);
+
+		}
+		#endregion
+
+		private void SubmitButton_Click(object sender, System.EventArgs e)
+		{
+			Label1.Text = TextBox1.Text;
+		}
+
+	}
+}

9d339c80bfcbcdd0a5fb8a2a1b4680884031849c
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-07-24 17:08:46 +0000

    2003-07-24  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* server/AcceptEncodingConfig.cs:
    	* server/AcceptEncodingModule.cs:
    	* server/AcceptEncodingSectionHandler.cs:
    	* server/GZipFilter.cs:
    	* server/Makefile: these .cs files have been moved to Mono.Http.dll.
    
    	* server/AssemblyInfo.cs: upgrade version.
    
    	* test/web.config: update to new type/assembly name and make it use
    	gzip by default.
    
    svn path=/trunk/xsp/; revision=16614

--- server/AssemblyInfo.cs
+++ server/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.2.99")]
+[assembly: AssemblyVersion("0.4.99")]
 [assembly: AssemblyTitle ("Mono-XSP Server")]
 [assembly: AssemblyDescription ("Minimalistic web server for testing System.Web")]
-[assembly: AssemblyCopyright ("2002 Ximian, Inc.")]
+[assembly: AssemblyCopyright ("2002,2003 Ximian, Inc.")]
 [assembly: AssemblyCompany ("Ximian, Inc.")]

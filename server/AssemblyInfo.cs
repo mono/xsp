98d2736d3cd1f7c60ad161100430f8159d5ab403
Miguel de Icaza <miguel@gnome.org>
2003-10-02 03:52:34 +0000

    Flushage
    svn path=/trunk/xsp/; revision=18507

--- server/AssemblyInfo.cs
+++ server/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.5.99")]
+[assembly: AssemblyVersion("0.6")]
 [assembly: AssemblyTitle ("Mono-XSP Server")]
 [assembly: AssemblyDescription ("Minimalistic web server for testing System.Web")]
 [assembly: AssemblyCopyright ("2002,2003 Ximian, Inc.")]

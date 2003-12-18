3118e97ade7fa3db90884e2f73566fa691b54a69
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2003-12-18 12:13:07 +0000

    2003-12-18  Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* standalone/Makefile.in: added test3.aspx
    	* standalone/test1.cs: moved other tests to this one.
    	* standalone/test3.aspx: New test.
    
    svn path=/trunk/xsp/; revision=21318

--- nunit-tests/standalone/test1.cs
+++ nunit-tests/standalone/test1.cs
@@ -1,5 +1,5 @@
 //
-// test1: bug51682 - Label ID and ClientID
+// test1
 //
 // Authors:
 //	Gonzalo Paniagua Javier (gonzalo@ximian.com)
@@ -19,17 +19,39 @@ namespace Mono.ASPNET
 	public class Test1 : WebFormTestCase
 	{
 		LabelTester label;
+		ButtonTester button;
 
 		protected override void SetUp() 
 		{
-			Browser.GetPage ("http://127.0.0.1:8080/test1.aspx");
-			label = new LabelTester ("Label1", CurrentWebForm);
 		}
 		
 		[Test]
 		public void Bug51682 ()
 		{
+			// bug51682 - Label ID and ClientID
+			Browser.GetPage ("http://127.0.0.1:8080/test1.aspx");
+			label = new LabelTester ("Label1", CurrentWebForm);
 			AssertEquals ("#01", "The Page.ClientID is:Hello", label.Text);
+		}
+
+		[Test]
+		public void Bug52334 ()
+		{
+			// bug52334 - columns not tracking view state
+			Browser.GetPage ("http://127.0.0.1:8080/test2.aspx");
+			button = new ButtonTester ("Button1", CurrentWebForm);
+			button.Click ();
+			label = new LabelTester ("Label1", CurrentWebForm);
+			AssertEquals ("#01", "field:test", label.Text);
+		}
+
+		[Test]
+		public void Bug50154 ()
+		{
+			// Unexpected OnClick raised for button
+			Browser.GetPage ("http://127.0.0.1:8080/test3.aspx");
+			label = new LabelTester ("Label1", CurrentWebForm);
+			AssertEquals ("#01", "", label.Text);
 		}
 	}
 }

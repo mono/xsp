5fef79f25629955975232a09eb39c0a6789a03b6
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2004-06-11 06:30:44 +0000

    2004-06-11 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* test/Makefile.am: added typedesc.*
    	* test/typedesc.(aspx|cs): sample test control showing how a Type that
    	has a TypeDescriptor can be saved/restored to/from ViewState.
    
    svn path=/trunk/xsp/; revision=29269

---
+++ test/typedesc.aspx
@@ -0,0 +1,11 @@
+<%@ language="C#" %>
+<%@ Register TagPrefix="Mono" Namespace="Mono.Controls" assembly="typedesc" %>
+<%@ Import namespace="Mono.Controls" %>
+<html>
+<head>
+<title>Property that needs TypeConverter</title>
+</head>
+<body>
+<Mono:WeirdControl id="weird" runat="server" WeirdObject="Passed!"/>
+</body>
+</html>
---
+++ test/typedesc.cs
@@ -0,0 +1,71 @@
+//
+// typedsc_property.cs: control with a property whose Type has a Typeconverter
+//
+// Authors:
+//	Gonzalo Paniagua Javier (gonzalo@ximian.com)
+//
+// Licensed under the terms of the GNU GPL
+//
+// Copyright (C) 2004 Novell, Inc (http://www.novell.com)
+//
+
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Globalization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Mono.Controls
+{
+	[ParseChildren(false)]
+	public class WeirdControl : Label
+	{
+		public MyObject WeirdObject {
+			get {
+				object o = ViewState ["WeirdObject"];
+				if (o == null)
+					return null;
+				return (MyObject) o;
+			}
+			set {
+				ViewState ["WeirdObject"] = value;
+				base.Text = value.Text;
+			}
+		}
+	}
+
+	[TypeConverter (typeof (MyObjectConverter))]
+	public class MyObject
+	{
+		string text;
+
+		public MyObject (string text)
+		{
+			this.text = text;
+		}
+
+		public string Text {
+			get { return text; }
+		}
+	}
+
+	public class MyObjectConverter : TypeConverter
+	{
+		public MyObjectConverter () {}
+
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			return (sourceType == typeof (string));
+		}
+
+		public override object ConvertFrom (ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+			if (!(value is string))
+				return base.ConvertFrom (context, culture, value);
+
+			return new MyObject ((string) value);
+		}
+	}
+}
+

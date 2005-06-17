c4100b3f33ad3ef270e84ce4073e8fed3cbbb412
Rafael Teixeira <monoman@gmail.com>
2005-06-17 18:15:54 +0000

    2005-06-17  Rafael Teixeira  <rafaelteixeirabr@hotmail.com> 	* tools/asp_state/asp_state.cs: find configuration filename to match the 	executable name and path plus ".config", that fix not finding the oldnamed 	"asp_state.exe.config" what make it work for asp-state2.exe, besides it was 	looking for the configuration file in the current dir, not the dir containing 	the executable. Also for first timers like me be a bit more verbose and don't 	start if some command line arguments are passed (I tried a asp-state --help 	and got baffled).
    svn path=/trunk/xsp/; revision=46162

--- tools/asp_state/asp_state.cs
+++ tools/asp_state/asp_state.cs
@@ -8,20 +8,72 @@
 //
 
 using System;
+using System.IO;
+using System.Reflection;
 using System.Runtime.Remoting;
 
 namespace Mono.ASPNET.Tools {
 
-        public class StateServer {
+	public class StateServer {
+	
+		private static string ServerName {
+			get {
+				return Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly ().CodeBase);
+			}				
+		}	
 
-                [STAThread]
-                public static void Main (string [] args)
-                {
-                        RemotingConfiguration.Configure ("asp_state.exe.config");
-                        Console.ReadLine ();
-                }
+		private static string configurationfile;		
+		private static string ConfigurationFileName {
+			get {
+				if (configurationfile == null) {
+					configurationfile = Assembly.GetEntryAssembly ().CodeBase + ".config";
+					if (configurationfile.StartsWith("file://"))
+						configurationfile = configurationfile.Substring(7);
+				}
+				return configurationfile;
+			}
+		}
+		
+		private static void ShowUsage()
+		{
+			Console.WriteLine(@"ERROR: {0} doesn't recognize any command line arguments!!!
+				
+Usage is:
+    {0}
+	
+It loads the remoting configuration file (will try from {1})
+and works until <Enter> is pressed.
+				", ServerName, ConfigurationFileName);
+		}
 
-        }
+		private static void ShowVerboseConfigurationInfo(string filename)
+		{
+			Console.WriteLine("Loaded configuration from {0} that contains", filename);
+			Console.WriteLine("=============================================");
+			try {
+				StreamReader sr = new StreamReader(filename);
+				Console.WriteLine(sr.ReadToEnd());
+				sr.Close();
+			} catch (Exception ex) {
+				Console.WriteLine("ERROR reading configuration file:\n" + ex.ToString());
+			}
+			Console.WriteLine("=============================================");
+		}
+
+ 		[STAThread]
+		public static void Main (string [] args)
+		{
+			if (args.Length == 0) {
+				RemotingConfiguration.Configure (ConfigurationFileName);
+				ShowVerboseConfigurationInfo(ConfigurationFileName);
+				Console.Write("Press <Enter> to stop...");
+				Console.ReadLine ();
+			} else {
+				ShowUsage();
+			}
+		}
+
+	}
 
 }
 

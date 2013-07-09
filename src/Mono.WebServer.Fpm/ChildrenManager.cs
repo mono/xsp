f76797329969bbe60bf86972e2dd7e7f4bf707dc
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2013-07-09 23:27:41 +0200

    [Fpm] Separate spawn

---
+++ src/Mono.WebServer.Fpm/ChildrenManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Mono.Unix;
+using Mono.WebServer.Log;
+
+namespace Mono.WebServer.Fpm
+{
+	static class ChildrenManager
+	{
+		static readonly List<ChildInfo> children = new List<ChildInfo>();
+
+		public static void KillChildren()
+		{
+			foreach (ChildInfo child in children) {
+				try {
+					if (!child.Process.HasExited)
+						child.Process.Kill();
+				}
+				catch (InvalidOperationException) {
+					// Died between the if and the kill
+				}
+			}
+		}
+
+		public static void TermChildren()
+		{
+			foreach (ChildInfo child in children) {
+				try {
+					if (!child.Process.HasExited)
+						; //TODO: Write some nice close code
+				} catch (InvalidOperationException) {
+					// Died between the if and the kill
+				}
+			}
+		}
+
+		public static void StartChildren(FileInfo[] configFiles, ConfigurationManager configurationManager)
+		{
+			foreach (var fileInfo in configFiles) {
+				Logger.Write(LogLevel.Debug, "Loading {0}", fileInfo.Name);
+				var childConfigurationManager = new ChildConfigurationManager();
+				childConfigurationManager.LoadXmlConfig(fileInfo.FullName);
+				string user = childConfigurationManager.User;
+				string fastCgiCommand = configurationManager.FastCgiCommand;
+
+				ChildInfo child;
+				if (Platform.IsUnix) {
+					if (String.IsNullOrEmpty(user)) {
+						Logger.Write(LogLevel.Warning, "Configuration file {0} didn't specify username, defaulting to file owner", fileInfo.Name);
+						user = UnixFileSystemInfo.GetFileSystemEntry(fileInfo.FullName).OwnerUser.UserName;
+					}
+
+					child = Spawner.RunAs(user, Spawner.SpawnChild, fileInfo, fastCgiCommand);
+				} else {
+					Logger.Write(LogLevel.Warning, "Configuration file {0} didn't specify username, defaulting to the current one", fileInfo.Name);
+					child = Spawner.SpawnChild(fileInfo, fastCgiCommand);
+				}
+				children.Add(child);
+			}
+		}
+	}
+}
---
+++ src/Mono.WebServer.Fpm/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Principal;
+
+namespace Mono.WebServer.Fpm
+{
+	static class Spawner 
+	{
+		public static T RunAs<T, T1, T2>(string user, Func<T1, T2, T> action, T1 arg0, T2 arg1)
+		{
+			using (var identity = new WindowsIdentity(user))
+			using (identity.Impersonate())
+				return action(arg0, arg1);
+		}
+
+		public static ChildInfo SpawnChild(FileInfo fileInfo, string fastCgiCommand)
+		{
+			var info = new ChildInfo {
+				Process = new Process {
+					StartInfo = new ProcessStartInfo {
+						FileName = fastCgiCommand,
+						Arguments = String.Format("--config-file {0}", fileInfo.FullName),
+						UseShellExecute = true
+					}
+				}
+			};
+			info.Process.Start();
+			return info;
+		}
+	}
+}

c715e7063a34d267a386c174981ecfce0ba153fd
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2013-07-12 20:39:13 +0200

    [Test] Test for passing tcp sockets

--- src/Mono.WebServer.Test/Socket.cs
+++ src/Mono.WebServer.Test/Socket.cs
@@ -5,36 +5,67 @@ using System.IO;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using Mono.WebServer.Fpm;
+using System.Net;
 
 namespace Mono.WebServer.Test {
-	[TestFixture()]
+	[TestFixture]
 	public class SocketTest
 	{
 		const string fastcgiSocketPath = "\0fastcgi";
 		const string fpmSocketPath = "\0fpm";
 		const string message = "Foo!";
 
-		[Test()]
-		public void TestPassFd ()
+		[Test]
+		public void TestPassUnixFd ()
 		{
-			Listen (fastcgiSocketPath, FastCgiAccept);
-			Listen (fpmSocketPath, FpmAccept);
-
-			UnixClient webserver = new UnixClient (fpmSocketPath);
-			string read;
-			using (var stream = webserver.GetStream ())
-			using (var reader = new StreamReader(stream))
-				read = reader.ReadLine ();
-			Assert.AreEqual (message, read);
+			using (ListenUnix (fastcgiSocketPath, FastCgiAccept))
+			using (ListenUnix (fpmSocketPath, FpmUnixAccept)) {
+				var webserver = new UnixClient (fpmSocketPath);
+				string read;
+				using (var stream = webserver.GetStream ())
+				using (var reader = new StreamReader(stream))
+					read = reader.ReadLine ();
+				Assert.AreEqual (message, read);
+			}
 		}
 
-		static void Listen (string path, AsyncCallback accept)
+		[Test]
+		public void TestPassTcpFd ()
 		{
-			UnixClient socket = new UnixClient ();
+			using (ListenUnix (fastcgiSocketPath, FastCgiAccept)) {
+				var portAndSocket = ListenTcp (FpmTcpAccept);
+
+				using (portAndSocket.Item2) {
+					var webserver = new TcpClient ();
+					webserver.Connect (IPAddress.Loopback, portAndSocket.Item1);
+					string read;
+					using (var stream = webserver.GetStream ())
+					using (var reader = new StreamReader(stream))
+						read = reader.ReadLine ();
+					Assert.AreEqual (message, read);
+				}
+			}
+		}
+
+		static IDisposable ListenUnix (string path, AsyncCallback accept)
+		{
+			var socket = new UnixClient ();
 			var client = socket.Client;
 			client.Bind (new UnixEndPoint (path));
 			client.Listen (1);
 			client.BeginAccept (accept, client);
+			return socket;
+		}
+
+		static Tuple<int,IDisposable> ListenTcp (AsyncCallback accept)
+		{
+			var socket = new TcpListener (IPAddress.Loopback, 0);
+			socket.Start ();
+			socket.BeginAcceptSocket (accept, socket);
+			var endpoint = socket.LocalEndpoint as IPEndPoint;
+			if (endpoint == null)
+				Assert.Fail ("LocalEndPoint wasn't an IPEndPoint");
+			return new Tuple<int,IDisposable>(endpoint.Port, socket.Server);
 		}
 
 		void FastCgiAccept(IAsyncResult res)
@@ -42,7 +73,7 @@ namespace Mono.WebServer.Test {
 			if (!res.IsCompleted)
 				throw new ArgumentException("res");
 
-			var socket = res.AsyncState as System.Net.Sockets.Socket;
+			var socket = res.AsyncState as Socket;
 			if (socket == null)
 				throw new ArgumentNullException ("state");
 
@@ -54,16 +85,31 @@ namespace Mono.WebServer.Test {
 			}
 		}
 
-		void FpmAccept(IAsyncResult res)
+		void FpmUnixAccept(IAsyncResult res)
 		{
 			if (!res.IsCompleted)
 				throw new ArgumentException("res");
 
-			var socket = res.AsyncState as System.Net.Sockets.Socket;
+			var socket = res.AsyncState as Socket;
 			if (socket == null)
 				throw new ArgumentNullException ("state");
 
 			using (var connection = socket.EndAccept (res)) {
+				UnixClient back = new UnixClient (fastcgiSocketPath);
+				SocketPassing.SendTo (back.Client, connection.Handle);
+			}
+		}
+
+		void FpmTcpAccept(IAsyncResult res)
+		{
+			if (!res.IsCompleted)
+				throw new ArgumentException("res");
+
+			var socket = res.AsyncState as TcpListener;
+			if (socket == null)
+				throw new ArgumentNullException ("state");
+
+			using (var connection = socket.EndAcceptSocket (res)) {
 				UnixClient back = new UnixClient (fastcgiSocketPath);
 				SocketPassing.SendTo (back.Client, connection.Handle);
 			}

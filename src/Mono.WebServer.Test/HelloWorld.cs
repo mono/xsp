b26fbd4c330b261c5f3beb16fcabd62a81914d26
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2013-08-02 16:12:59 +0200

    [WebServer + Test] Cleaner tests

--- src/Mono.WebServer.Test/HelloWorld.cs
+++ src/Mono.WebServer.Test/HelloWorld.cs
@@ -13,40 +13,19 @@ namespace Mono.WebServer.Test
 		[SetUp]
 		public void Init ()
 		{
-			// Force loading of the XSP assembly
-			new SecurityConfiguration ();
-			// and the WebServer one
-			HttpErrors.BadRequest();
-
-			string binpath = Path.Combine (Environment.CurrentDirectory, "bin");
-			if (!Directory.Exists (binpath))
-				Directory.CreateDirectory (binpath);
-
-			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies ();
-			foreach (Assembly assembly in assemblies)
-				MaybeCopyAssembly (assembly, binpath);
-		}
-
-		static void MaybeCopyAssembly (Assembly assembly, string binpath)
-		{
-			if (assembly.GlobalAssemblyCache || assembly.CodeBase == null)
-				return;
-
-			string cut = assembly.CodeBase.Substring (Platform.IsUnix ? 7 : 8);
-			string filename = Path.GetFileName (cut);
-
-			string target = Path.Combine (binpath, filename);
-			File.Copy (cut, target, true);
+			Utilities.LoadAssemblies ();
+			Utilities.CopyLoadedAssemblies ();
+			Utilities.SetLogToFail ();
 		}
 
 		[Test]
 		public void TestCase ()
 		{
-			var result = Server.Main (new []{"--applications", "/:.","--port", "9000","--nonstop"});
+			int result = Server.Main (new []{"--applications", "/:.","--port", "9000","--nonstop"});
 			Assert.AreEqual (0, result);
 			var wc = new WebClient ();
 			try {
-				var downloaded = wc.DownloadString ("http://localhost:9000/");
+				string downloaded = wc.DownloadString ("http://localhost:9000/");
 				Assert.AreEqual (Environment.CurrentDirectory, downloaded);
 			} catch (WebException e) {
 				Assert.Fail(e.Message);
---
+++ src/Mono.WebServer.Test/Stress.cs
@@ -0,0 +1,70 @@
+using System;
+using NUnit.Framework;
+using Mono.WebServer.XSP;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Mono.WebServer.Test {
+	public class Stress
+	{
+		[SetUp]
+		public void Init ()
+		{
+			Utilities.LoadAssemblies ();
+			Utilities.CopyLoadedAssemblies ();
+			Utilities.SetLogToFail ();
+		}
+
+		[Test]
+		public void ValidConnections ()
+		{
+			int result = Server.Main (new []{"--applications", "/:.","--port", "9000","--nonstop"});
+			Assert.AreEqual (0, result);
+			const int count = 1000;
+			var threads = new Thread [count];
+			try {
+				for(int i = 0; i < count; i++) {
+					threads [i] = new Thread (() => {
+						var wc = new WebClient ();
+						string downloaded = wc.DownloadString ("http://localhost:9000/");
+						Assert.AreEqual (Environment.CurrentDirectory, downloaded);
+					});
+					threads [i].Start ();
+				}
+
+				foreach (Thread thread in threads)
+					thread.Join();
+			} catch (WebException e) {
+				Assert.Fail(e.Message);
+			}
+		}
+
+		[Test]
+		public void InvalidConnections ()
+		{
+			int result = Server.Main (new []{"--applications", "/:.","--port", "9000","--nonstop"});
+			Assert.AreEqual (0, result);
+			const int count = 1000;
+			var threads = new Thread [count];
+			try {
+				for(int i = 0; i < count; i++) {
+					threads [i] = new Thread (() => {
+						TcpClient client = new TcpClient ("localhost", 9000);
+						using(var sw = new StreamWriter(client.GetStream()))
+							sw.Write("\0\0\0\0");
+						client.Close ();
+					});
+					threads [i].Start ();
+				}
+
+				foreach (Thread thread in threads)
+					thread.Join();
+			} catch (WebException e) {
+				Assert.Fail(e.Message);
+			}
+		}
+	}
+}
+
